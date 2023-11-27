92713f5ead9480222b3fd9c62d0c1bf51264dc5c
vivian <92966837+abigailviviannn@users.noreply.github.com>
2023-11-27 13:32:00 +0700

    Add files via upload

--- components/card/index.js
+++ components/card/index.js
@@ -1,1 +1,26 @@
-c
+import React from "react";
+import "./index.css";
+
+export default function index({ data, onClick }) {
+    return (
+        <div className="card" onClick={onClick}>
+            {data ? (
+                <>
+                    <figure>
+                        {data && data.thumbnail ? (
+                            <img src={data.thumbnail} alt={data.title} />
+                        ) : (
+                            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d1/Image_not_available.png/640px-Image_not_available.png" alt="Not Found"></img>
+                        )}
+                    </figure>
+                    <div className="card-info">
+                        <h3>{data.title}</h3>
+                        <p>{data.genre}</p>
+                    </div>
+                </>
+            ) : (
+                <p>Loading...</p>
+            )}
+        </div>
+    );
+}
